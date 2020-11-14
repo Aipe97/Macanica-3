e917d9d1b5098b7ae0617ece113e1b2ff476a91f
Gabriel <gad2510@hotmail.com>
2020-11-14 17:21:07 -0600

    Objetos

---
+++ Macanica parcial 3/Assets/script_managerRebotes.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_managerRebotes : MonoBehaviour
+{
+    public static script_managerRebotes instance;
+
+    script_plataformas plataformas;
+
+    Rigidbody selfRigid;
+    [SerializeField]
+    float fuerza = 200f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        selfRigid = GetComponent<Rigidbody>();
+        selfRigid.AddForce(Vector3.right* fuerza, ForceMode.Force);
+        //plataformas = GameObject.FindObjectsOfType<script_plataformas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void variarEscalas()
+    {
+
+        /*print(plataformas.Length);
+        foreach(script_plataformas plat in plataformas)
+        {
+            plat.CambiarEscala();
+        }*/
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (plataformas != null)
+        {
+            script_plataformas temp = collision.gameObject.GetComponent<script_plataformas>();
+
+            if (temp != null && temp!=plataformas)
+            {
+                plataformas.CambiarEscala();
+
+                plataformas = temp;
+            }
+        }
+
+        plataformas = collision.gameObject.GetComponent<script_plataformas>();
+    }
+}
---
+++ Macanica parcial 3/Assets/script_plataformas.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_plataformas : MonoBehaviour
+{
+    Vector3 escalaInicial;
+
+    float rangoVaricion=2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        escalaInicial = transform.parent.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambiarEscala()
+    {
+        float cambio = Random.Range(0f, rangoVaricion);
+        Vector3 temp = escalaInicial;
+        temp.y += cambio;
+        print(temp);
+        transform.parent.localScale = temp;
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            script_managerRebotes.instance.variarEscalas();
+        }   
+    }*/
+}
