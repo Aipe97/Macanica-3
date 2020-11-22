e73da999e4e4b14a5cb40f83e4d037996cb973e5
Gabriel <gad2510@hotmail.com>
2020-11-22 16:19:14 -0600

    Update

---
+++ Assets/transformar_pelota.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformar_pelota : MonoBehaviour
+{
+    public Rigidbody otherBall;
+    Vector3 otherInitPos;
+    Collider otherColl, plCollider;
+    PhysicMaterial playerMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            plCollider = other.GetComponent<Collider>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
