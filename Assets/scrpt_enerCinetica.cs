e0c4c1c9a66761c56b4670fcb250e5eac75add68
Aipe97 <aipe84@yahoo.com.mx>
2020-11-22 14:54:18 -0600

    codigo energia cinetica

---
+++ Assets/scrpt_enerCinetica.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrpt_enerCinetica : MonoBehaviour
+{
+    public Text valorCinetica;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        valorCinetica.text = "(1/2)mv2=" + ((collision.gameObject.GetComponent<Rigidbody>().mass * collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude) / 2).ToString("F2") + "J";
+    }
+}
