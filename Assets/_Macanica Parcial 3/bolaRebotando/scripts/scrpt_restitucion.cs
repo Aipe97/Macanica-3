1367a22e114cc0da1d95eef093d8ffcb04f69312
Aipe97 <aipe84@yahoo.com.mx>
2020-11-23 19:38:54 -0600

    implementacion de restitucion

--- Assets/_Macanica Parcial 3/bolaRebotando/scripts/scrpt_restitucion.cs
+++ Assets/_Macanica Parcial 3/bolaRebotando/scripts/scrpt_restitucion.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 
 public class scrpt_restitucion : MonoBehaviour
 {
-    public Text textoRestitucion;  
+    Text textoRestitucion;  
     float velocidad1, velocidad2, mew1, mew2;
     
 
     private void Start()
     {
         textoRestitucion = GameObject.FindGameObjectWithTag("TextoRestitucion").GetComponent<Text>();
-        textoRestitucion.text = "Choque " + gameObject.GetComponent<script_managerRebotes>().currentType;
+        textoRestitucion.text = "" + gameObject.GetComponent<script_managerRebotes>().currentType;
     }
 
 
@@ -32,13 +32,13 @@ public class scrpt_restitucion : MonoBehaviour
         {
             mew1 = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
             mew2 = collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
-            textoRestitucion.text = "Coeficiente de restitucion \n e=" + Mathf.Abs((mew2 - mew1 / velocidad1 - velocidad2));
+            textoRestitucion.text = "Restitución \n e=" + Mathf.Abs((mew2 - mew1 / velocidad1 - velocidad2));
 
-            print("antes " + velocidad1);
+            /*print("antes " + velocidad1);
             print("despues " + mew1);
 
             print("antes2 " + velocidad2);
-            print("despues2 " + mew2);
+            print("despues2 " + mew2);*/
         }
     }
 }
