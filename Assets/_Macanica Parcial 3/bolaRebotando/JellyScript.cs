f345093cedb3396c88ace8812308ef717debc61b
Gabriel <gad2510@hotmail.com>
2020-12-04 18:10:27 -0600

    algo

--- Assets/_Macanica Parcial 3/bolaRebotando/JellyScript.cs
+++ Assets/_Macanica Parcial 3/bolaRebotando/JellyScript.cs
@@ -4,15 +4,121 @@ using UnityEngine;
 
 public class JellyScript : MonoBehaviour
 {
+    [SerializeField]
+    float bounceSpeed;
+    [SerializeField]
+    float fallForce;
+    [SerializeField]
+    float stiffness;
+
+    private MeshFilter meshF;
+    private Mesh mesh;
+
+    JellyVertex[] jellyVertices;
+    Vector3[] currentMeshVertices;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        meshF = GetComponent<MeshFilter>();
+        mesh = meshF.mesh;
+        GetVertices();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateVerteces();
+    }
+
+    private void GetVertices()
+    {
+        jellyVertices = new JellyVertex[mesh.vertices.Length];
+        currentMeshVertices = new Vector3[mesh.vertices.Length];
+
+        for(int i = 0; i < mesh.vertices.Length; i++)
+        {
+            jellyVertices[i] = new JellyVertex(i, mesh.vertices[i]);
+            currentMeshVertices[i] = mesh.vertices[i];
+        }
+    }
+
+    private void UpdateVerteces()
+    {
+        for (int i = 0; i < jellyVertices.Length; i++)
+        {
+            jellyVertices[i].UpdateVel(bounceSpeed);
+            jellyVertices[i].Settle(stiffness);
+
+            jellyVertices[i].nowVetexPos += jellyVertices[i].nowVelocity * Time.deltaTime;
+            print("Antes: " + currentMeshVertices[i]);
+            currentMeshVertices[i] = jellyVertices[i].nowVetexPos;
+            print("Despues: "+currentMeshVertices[i]);
+        }
+
+        mesh.vertices = currentMeshVertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint[] collisionPts = collision.contacts;
+        for(int i = 0; i < collisionPts.Length; i++)
+        {
+            Vector3 inputPts = collisionPts[i].point + (collisionPts[i].point * 0.1f);
+            print(inputPts + " index: "+ i);
+            ApplyPressere2Point(inputPts, fallForce);
+            
+        }
+    }
+
+    public void ApplyPressere2Point(Vector3 inputPt,float force)
+    {
+        for(int i = 0; i < jellyVertices.Length; i++)
+        {
+            jellyVertices[i].ApplyPress(transform, inputPt, force);
+        }
+    }
+
+    public class JellyVertex
+    {
+        public int vertexIndex;
+        public Vector3 initVetexPos;
+        public Vector3 nowVetexPos;
+
+        public Vector3 nowVelocity;
+
+        public JellyVertex(int _vetexI, Vector3 _initVertexP)
+        {
+            vertexIndex = _vetexI;
+            initVetexPos = _initVertexP;
+            nowVetexPos = _initVertexP;
+            nowVelocity = Vector3.zero;
+        }
+
+        public Vector3 GetNowDisplacement()
+        {
+            return nowVetexPos - initVetexPos;
+        }
+
+        public void UpdateVel(float _bounce)
+        {
+            nowVelocity = nowVelocity - GetNowDisplacement() * _bounce * Time.deltaTime;
+        }
+
+        public void Settle(float _stiffness)
+        {
+            nowVelocity *= 1.0f - _stiffness * Time.deltaTime;
+        }
+
+        public void ApplyPress(Transform _transform, Vector3 _position, float _pressure)
+        {
+            Vector3 distanceVerPos = nowVetexPos - _transform.InverseTransformPoint(_position);
+            float adaptPress = _pressure / (1.0f + distanceVerPos.sqrMagnitude);
+            float vel = adaptPress * Time.deltaTime;
+            nowVelocity += distanceVerPos.normalized * vel;
+        }
     }
 }
---
+++ Assets/_Macanica Parcial 3/masaGravedad/calculo_gravedad.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class calculo_gravedad : MonoBehaviour
+{
+    public Transform objetoDerecho, objetoIzquierdo;
+    public Text formula;
+
+    float coeficienteGraveda;
+    float fuerzaGravedad;
+    public float peso1, peso2;
+    float distancia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coeficienteGraveda = 6.67f *Mathf.Pow(10, -11);
+        print(coeficienteGraveda);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distancia = Vector3.Distance(objetoDerecho.position, objetoIzquierdo.position);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Cambio");
+            float diferencia = distancia;
+            distancia *= 3;
+
+            objetoDerecho.position += Vector3.right * diferencia;
+            objetoIzquierdo.position += Vector3.left * diferencia;
+
+            peso1 = peso1 * 3;
+
+        }
+
+       
+        fuerzaGravedad = (coeficienteGraveda * peso1 * peso2) / Mathf.Pow(distancia, 2);
+        formula.text =String.Format("Fg= (G*m1*m2)/d^2 \n {0} = ({1}*{2}*{3})/{4}^2", fuerzaGravedad.ToString("F2"),coeficienteGraveda.ToString("F2"), peso1,peso2,distancia.ToString("F2"));
+    }
+}
