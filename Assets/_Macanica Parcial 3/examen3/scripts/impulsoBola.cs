e5e8afa825718065ec55e3fdfd5fdeb01956348c
Aipe97 <aipe84@yahoo.com.mx>
2020-12-08 16:20:02 -0600

    implementacion de slider

--- Assets/_Macanica Parcial 3/examen3/scripts/impulsoBola.cs
+++ Assets/_Macanica Parcial 3/examen3/scripts/impulsoBola.cs
@@ -1,33 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class impulsoBola : MonoBehaviour
 {
-    int numeroParaMeterSeno;
-    public float numeroImpulsador;
+    
+    public float f_fuerzaPostSeno;
+    float f_valorSeno;
     Rigidbody rb_bolaBlanca;
     Transform t_posicionCamara;
-    Vector3 v3_fuerzaPaPegar;
+    Slider ui_slider;
 
     // Start is called before the first frame update
     void Start()
     {
         rb_bolaBlanca = GetComponent<Rigidbody>();
-        t_posicionCamara = GameObject.FindGameObjectWithTag("posicionCamara").transform;        
+        t_posicionCamara = GameObject.FindGameObjectWithTag("posicionCamara").transform;
+        ui_slider = GameObject.FindGameObjectWithTag("slider").GetComponent<Slider>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        numeroImpulsador =1-Mathf.Abs(Mathf.Sin(Time.time));
+        f_valorSeno = Mathf.Sin(Time.time);
+        f_fuerzaPostSeno = 1 - Mathf.Abs(f_valorSeno);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb_bolaBlanca.AddForce(t_posicionCamara.forward* numeroImpulsador * 100f);
+            rb_bolaBlanca.AddForce(t_posicionCamara.forward* f_fuerzaPostSeno * 100f);
             
         }
 
         //print(numeroImpulsador);
+
+        ui_slider.value = Mathf.Clamp(f_valorSeno + 1, 0, 2); //60 es el valor máximo
     }
 }
