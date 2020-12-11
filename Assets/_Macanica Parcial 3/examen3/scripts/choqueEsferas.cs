c13fed16d3b4efddd366fce092ae955883a852f2
Aipe97 <aipe84@yahoo.com.mx>
2020-12-11 17:01:19 -0600

    agregar sonidos

--- Assets/_Macanica Parcial 3/examen3/scripts/choqueEsferas.cs
+++ Assets/_Macanica Parcial 3/examen3/scripts/choqueEsferas.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class choqueEsferas : MonoBehaviour
 {
+
+    //public AudioClip impactado;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GetComponent<AudioSource>().clip = impactado;
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
         
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("esfera"))
+            GetComponent<AudioSource>().Play();
+    }
+
+
 }
