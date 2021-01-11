7f10b8af598fc4b96c99a8d3efb418c3f1ae6791
Gabriel <gad2510@hotmail.com>
2021-01-10 22:44:50 -0600

    algo de mecanica

---
+++ Assets/_MecanicaFinal/ProyectoFinal/Scene_Manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_Manager : MonoBehaviour
+{
+    List<scrpt_MovimientoArmonico> pelotas;
+    public scrpt_MovimientoArmonico refObjeto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pelotas = new List<scrpt_MovimientoArmonico>();
+        scrpt_MovimientoArmonico primera = GameObject.FindGameObjectWithTag("esfera").GetComponent<scrpt_MovimientoArmonico>();
+        pelotas.Add(primera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Equals) && pelotas.Count<5)
+        {
+            CrearPelota();
+        }
+    }
+
+    void CrearPelota()
+    {
+        int pos;
+        Vector3 dir= ChecarPosisionYDireccion(out pos);
+        scrpt_MovimientoArmonico pelota= Instantiate(refObjeto, dir * pos, Quaternion.identity);
+
+        pelotas.Add(pelota);
+    }
+
+    Vector3 ChecarPosisionYDireccion(out int index)
+    {
+        int count = pelotas.Count+1;
+        Vector3 dir= (count % 2 == 0)?Vector3.right:Vector3.left;
+        index = count / 2;
+
+        return dir*1.2f;
+    }
+}
+
---
+++ Assets/_MecanicaFinal/ProyectoFinal/scrpt_MovimientoArmonico.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrpt_MovimientoArmonico : MonoBehaviour
+{
+    public Gradient colorPerSec;
+    LineRenderer linea;
+
+    public bool randomAmplitud;
+
+    public float peso;
+    public float amplitud;
+
+    public float velocidad, velocidadMax;
+    public float aceleracion, aceleracionMax;
+    public float position;
+
+    public float variable_hooke;
+    public float frecuenciaCiclica;
+
+    float initTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IniciarVariables();
+
+        linea = GetComponent<LineRenderer>();
+        linea.positionCount=2;
+
+        initTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CambiarVariables();
+
+        SetColorDeLinea();
+    }
+
+    private void IniciarVariables()
+    {
+        amplitud = Random.Range(0.1f, 10.0f);
+        peso = Random.Range(0.1f, 10.0f);
+
+        variable_hooke = peso * 9.81f / amplitud;
+        frecuenciaCiclica = Mathf.Sqrt(variable_hooke / peso);
+
+        aceleracionMax = Mathf.Pow(frecuenciaCiclica, 2) * amplitud;
+        velocidadMax = frecuenciaCiclica * amplitud;
+    }
+
+    private void CambiarVariables()
+    {
+        float angulo = (frecuenciaCiclica * (Time.time-initTime)) - 90f;
+
+        position = (amplitud * Mathf.Sin(angulo)) + amplitud;
+        Vector3 temp = transform.position;
+        temp.y= position;
+        transform.position = temp;
+
+        aceleracion = -amplitud * frecuenciaCiclica * Mathf.Sin(angulo);
+        velocidad = amplitud * frecuenciaCiclica * Mathf.Cos(angulo);
+    }
+
+    private void SetColorDeLinea()
+    {
+        Color col = colorPerSec.Evaluate(Mathf.Abs(velocidad/velocidadMax));
+
+        linea.SetPosition(0, transform.position);
+        Vector3 direccion =  (Vector3.up* velocidad)+ transform.position;
+        linea.SetPosition(1, direccion);
+
+        linea.material.color = col;
+    }
+}
