9b3ff93cef2bcae93aa7728337fb0ef3d1de1fc4
Gabriel <gad2510@hotmail.com>
2021-01-14 20:49:59 -0600

    [Fix] zoom de la camara

--- Assets/_MecanicaFinal/ProyectoFinal/scrpt_zoomCamara.cs
+++ Assets/_MecanicaFinal/ProyectoFinal/scrpt_zoomCamara.cs
@@ -6,15 +6,17 @@ public class scrpt_zoomCamara : MonoBehaviour
 {
     Transform esfera;
     bool zoomActivo = false;
-    GameObject mainCamera;
+    public bool zoomInOut=true;
+    Transform mainCamera;
     Vector3 posOriginal;
 
+    float counter = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        mainCamera = Camera.main.transform;
         posOriginal = GameObject.FindGameObjectWithTag("posicionCamara").transform.position;
-        esfera = gameObject.transform.GetChild(1); //guarda el tranform del hijo [1] o sea la esfera
+        esfera = GetComponentInChildren<scrpt_MovimientoArmonico>().transform; //guarda el tranform del hijo [1] o sea la esfera
     }
 
     // Update is called once per frame
@@ -22,22 +24,21 @@ public class scrpt_zoomCamara : MonoBehaviour
     {
         if (zoomActivo)
         {
-            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, esfera.position + new Vector3(0, 0, -2.0f), 0.2f); //hace que la camara siga a la pelota
+            float dir = (zoomInOut) ? Time.deltaTime : -Time.deltaTime;
+            counter += dir;
+            mainCamera.position = Vector3.Lerp(esfera.position + new Vector3(0, 0, -2.0f), posOriginal, 1 - counter);//regresa la camara a la posicion original
+            counter = Mathf.Clamp(counter, 0.0f, 1.0f);
+
+            zoomActivo = counter > 0f;
         }
-        else if (!zoomActivo && mainCamera.transform.position != posOriginal)
-        {
-            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, posOriginal, 0.1f);//regresa la camara a la posicion original
-        }
+
     }
     void OnMouseDown()
     {
+        zoomInOut = !zoomActivo;
         if (!zoomActivo)
         {
-            zoomActivo = true;           
-        }
-        else
-        {
-            zoomActivo = false;          
-        }              
+            zoomActivo = true;
+        }      
     }
 }
